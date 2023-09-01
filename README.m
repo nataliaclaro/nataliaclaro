24825b1e5c4dc194e47c229d4d46910b82d316b1
nataliaclaro <143825289+nataliaclaro@users.noreply.github.com>
2023-09-01 12:35:53 -0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,9 @@
+- 👋sou natalia
+- tenho 16 years 
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+nataliaclaro/nataliaclaro is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
